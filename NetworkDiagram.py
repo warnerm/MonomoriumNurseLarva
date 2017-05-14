87c870580eb980e78a8757e82961f598c33d8727
mrwarner09 <mrwarner09@gmail.com>
2017-05-14 21:48:35 +0900

    three circles

--- NetworkDiagram.py
+++ NetworkDiagram.py
@@ -5,22 +5,23 @@ import math as m
 plt.figure(figsize=(6,6),facecolor='white')
 plt.subplot(111)
 
-n=100
+n=10
 cx = cy = 0.5
-r = 0.25
+r = 0.1
 center = [[0.5,0.33,0.67],[0.33,0.67,0.67]]
 
 
 
 def Addcircles():
+	for i in range(3):
+		AddOneType(center[0][i],center[1][i],r,n)
+	return
+
+def AddOneType(cx, cy, r, n):
 	P = CircleCoord (cx,cy,r,n)
 	S = np.linspace(50,50,n*2)
 	scat = plt.scatter(P[:,0],P[:,1],s=S,lw = 0.5)
-	plt.axis('off')
-	plt.show()
 	return
-
-
 
 def CircleCoord ( cx, cy, r, n):
 	X1 = np.fromfunction(lambda i, j: cx - r*np.cos((i*np.pi/(n))), (n,1),dtype=float)
@@ -36,7 +37,8 @@ def CircleCoord ( cx, cy, r, n):
 Addcircles(); #Add circles for genes
 #Addnames(); #Add gene names
 #Addlinks(); #Add arrows
-
+plt.axis('off')
+plt.show()
 
 
 
