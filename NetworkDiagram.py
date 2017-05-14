6ed075db37f0e6de6388f979a2360c3104272686
mrwarner09 <mrwarner09@gmail.com>
2017-05-14 22:43:48 +0900

    Added Dummy Labels

--- NetworkDiagram.py
+++ NetworkDiagram.py
@@ -7,23 +7,26 @@ plt.subplot(111)
 
 n=3
 circSize = 2000
+textOffset = 0.05
 r = 0.35
 center = [[0.5,0,1],[0.1,1,1]]
 colors = ["#000000", "#E69F00", "#56B4E9"]
+Genes = ["JH1","JH2","JH3","JH4","JH5","JH6"]
+Genes = [Genes,Genes,Genes]
 
 
 def Addcircles():
 	for i in range(3):
-		AddOneType(center[0][i],center[1][i],r,n,colors[i])
+		AddOneType(center[0][i],center[1][i],colors[i])
 	return
 
-def AddOneType(cx, cy, r, n,color):
-	P = CircleCoord (cx,cy,r,n)
+def AddOneType(cx, cy,color):
+	P = CircleCoord (cx,cy)
 	S = np.linspace(circSize,circSize,n*2)
 	scat = plt.scatter(P[:,0],P[:,1],s=S,lw = 0.5,marker="^",color=color)
 	return
 
-def CircleCoord ( cx, cy, r, n):
+def CircleCoord ( cx, cy):
 	X1 = np.fromfunction(lambda i, j: cx - r*np.cos((i*np.pi/(n))), (n,1),dtype=float)
 	X2 = np.fromfunction(lambda i, j: cx - r*np.cos(((n-i)*np.pi/(n))), (n,1),dtype=float)
 	X = np.concatenate((X1,X2),axis=0)
@@ -33,9 +36,21 @@ def CircleCoord ( cx, cy, r, n):
 	P = np.hstack((X,Y))
 	return P
 
+def Addnames ():
+	for i in range(3):
+		addLabels(i);
+	return
+
+def addLabels (Type):
+	P = CircleCoord (center[0][Type],center[1][Type])
+	for i in range(6):
+		plt.text(P[i][0],P[i][1]-textOffset,Genes[Type][i],
+			fontsize=16,horizontalalignment='center',
+			verticalalignment='center',color="w")
+	return
 
 Addcircles(); #Add circles for genes
-#Addnames(); #Add gene names
+Addnames(); #Add gene names
 #Addlinks(); #Add arrows
 plt.axis('off')
 plt.show()
