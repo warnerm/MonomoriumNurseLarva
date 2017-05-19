111ed9355f1e35ffb4a5a03a932f289ff2252617
mrwarner09 <mrwarner09@gmail.com>
2017-05-19 14:51:26 +0900

    Fixed errors, started running larval cluster

--- RandomNetworks_parallel.R
+++ RandomNetworks_parallel.R
@@ -6,7 +6,7 @@ library(parallel)
 library(plyr)
 
 
-load("~/Dropbox/monomorium nurses/data.processed/cleandata.RData")
+load("cleandata.RData")
 fpkm = log(fpkm + sqrt(fpkm ^ 2 + 1)) #hyperbolic sine transformation to normalize gene expression data
 
 #Create many random networks for a given sample set
@@ -15,31 +15,24 @@ RandomNetworks <- function(name){
   return(file)
 }
 
-summarize <- function(lst) {
-  lapply(lst, function(elt) {
-    if (elt$status=="ok") elt$value
-    else NA
-  })
-}
 
 
 
 #Parallel wrapper for genie function
 parallelGenie <- function(name){
   nReps = floor(boots/bootsPerCore)
-  no_cores <- detectCores() - 1
-  if (no_cores > 32){
-    no_cores = 32
-  }
-  # Initiate cluster
-  cl <- makeCluster(no_cores)
+ 
+  # Initiate cluster; this only works on linux
+  cl <- makePSOCKcluster(24,
+                         master=system("hostname -i", intern=TRUE))
+  
   clusterExport(cl = cl, c(
-                  "boots","nGene","input","runGenie","bootsPerCore")) ##Must export these variables for parLapply to see them
+                  "runGenie","nGene","input","runGenie","bootsPerCore")) ##Must export these variables for parLapply to see them
   
   # In parallel, go through all permutations
-  Results <- parLapply(cl,1:nReps, function(k) {
+  system.time(Results <- parLapply(cl,1:nReps, function(k) {
     runGenie(k)
-  })
+  }))
   stopCluster(cl)
   return(Results)
 }
@@ -48,8 +41,7 @@ parallelGenie <- function(name){
 #This is better than full parallelization so we can deal with the try error
 runGenie <- function(run){
   Results = list()
-  setwd("~/Downloads/GENIE3_R_C_wrapper")
-  source("~/Downloads/GENIE3_R_C_wrapper/GENIE3.R")
+  source("GENIE3.R")
   library(plyr)
   i = 1
   while (i <= bootsPerCore){
@@ -65,14 +57,19 @@ runGenie <- function(run){
   return(Results)
 }
 
-boots = 100000
+setwd("GENIE3_R_C_wrapper")
+source("GENIE3.R")
+
+boots = 500000
 nGene = 10
-bootsPerCore = 1000
+bootsPerCore = 10
 d <- fpkm
 input = d[,grepl("LS|W.*_L",colnames(d))]
 rownames(input) = rownames(fpkm)
 larva = RandomNetworks("Larva")
-save(larva,file="LarvaGenieParallel.RData")
+save(larva,file="LarvaGenieParallel1.RData")
+larva2 = RandomNetworks("Larva")
+save(larva2,file="LarvaGenieParallel2.RData")
 
 # codes = c("W.*_L","C.*WH","C.*WG")
 # names = c("WorkLarv","WorkNurseH","WorkNurseG")
