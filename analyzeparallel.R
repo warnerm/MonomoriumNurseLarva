bcf1f1a4eb7f67cb623124debf1e3e89e92cdc61
mrwarner09 <mrwarner09@gmail.com>
2017-07-18 15:54:30 -0400

    Beginning mock-up figures

--- analyzeparallel.R
+++ analyzeparallel.R
@@ -93,10 +93,40 @@ d2$tissue[grepl("WH",d2$variable)]="WorkerHead"
 d2$connection.type = "within tissue"
 d2$connection.type[grepl("between",d2$variable)]="between tissue"
 
-png("~/Writing/Figures/NurseLarva/Fig4a.png",width=3000,height=3000,res=300)
+png("~/Writing/Figures/NurseLarva/Fig1a.png",width=3000,height=3000,res=300)
 ggplot(d2,aes(x=tissue,y=value,fill=connection.type))+geom_boxplot()+
   theme_bw()+xlab("tissue")+ylab("connection strength")+
   scale_fill_grey(start = 0.5, end = 0.8,name="connection type")+
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=20),
+        legend.text=element_text(size=15),
+        legend.title=element_text(size=20))
+dev.off()
+
+png("~/Writing/Figures/NurseLarva/Fig1b.png",width=3000,height=3000,res=300)
+ggplot(dfAll,aes(x=WHwithin,y=WHbetween))+geom_point()+
+  theme_bw()+xlab("within")+ggtitle("WH connection strength")+
+  ylab("between")+geom_abline(slope=1)+
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=20),
+        legend.text=element_text(size=15),
+        legend.title=element_text(size=20))
+dev.off()
+
+png("~/Writing/Figures/NurseLarva/Fig1c.png",width=3000,height=3000,res=300)
+ggplot(dfAll,aes(x=WGwithin,y=WGbetween))+geom_point()+
+  theme_bw()+xlab("within")+ggtitle("WG connection strength")+
+  ylab("between")+geom_abline(slope=1)+
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=20),
+        legend.text=element_text(size=15),
+        legend.title=element_text(size=20))
+dev.off()
+
+png("~/Writing/Figures/NurseLarva/Fig1d.png",width=3000,height=3000,res=300)
+ggplot(dfAll,aes(x=Lwithin,y=Lbetween))+geom_point()+
+  theme_bw()+xlab("within")+ggtitle("L connection strength")+
+  ylab("between")+geom_abline(slope=1)+
   theme(axis.text=element_text(size=15),
         axis.title=element_text(size=20),
         legend.text=element_text(size=15),
@@ -169,4 +199,97 @@ ggplot(d[1:5,],aes(x=meanSI,y=meanF,color=PS2))+geom_point(size=8,shape=1,stroke
         legend.text=element_text(size=15),legend.title=element_text(size=20))
 dev.off()
 
-ggplot
+####EdgeR
+library(edgeR)
+load("~/Dropbox/monomorium nurses/data.processed/cleandata.RData")
+EdgeR <- function(data,design,coef){
+  data <- DGEList(counts=data)
+  data <- calcNormFactors(data)
+  dat <- estimateGLMTrendedDisp(data, design)
+  dat <- estimateGLMTagwiseDisp(dat, design)
+  fit <- glmFit(dat,design)
+  diff <- glmLRT(fit, coef=coef) 
+  out <- topTags(diff,n=Inf,adjust.method="BH")$table 	###Calculate FDR
+  return(out)
+}
+
+logFCconn <- function(code){
+  df <- counts[,grepl(code,colnames(counts))]
+  f <- factors[rownames(factors) %in% colnames(df),]
+  design <- model.matrix(~Caste,data=droplevels(f))
+  a <- EdgeR(df,design,2)
+  a$Gene = rownames(a)
+  d <- merge(dfAll,a,by="Gene")
+  ggplot(d,aes(x=SIWH,y=logFC))+geom_point()
+}
+fpkm = log(fpkm + sqrt(fpkm ^ 2 + 1)) #hyperbolic sine transformation to normalize gene expression data
+
+meanQH = rowSums(fpkm[,grepl("QH",colnames(fpkm))])
+meanQG = rowSums(fpkm[,grepl("QG",colnames(fpkm))])
+
+expr = data.frame(QH=meanQH,QG=meanQG,Gene=names(meanQH))
+d = merge(dfAll,expr,by="Gene")
+lm <- glm(log(f.est) ~ PS2 + QH + QG + WHwithin + WGwithin+
+            WHbetween + WGwithin +
+            Lwithin + Lbetween,data=d)
+
+lm <- glm(log(f.est) ~ PS2 + QH + QG + SIWH + SIWG + SIL,data=d)
+drop1(lm,.~.,test="Chi") 
+summary(glht(lm, mcp(PS2="Tukey"))) 
+
+lm <- glm(log(f.est) ~  SIWH + SIWG,data=d)
+lm <- glm(SIWG ~ PS2,data=d)
+
+dfAllH <- merge(dfAll,df,)
+
+df <- counts[,grepl("QG|WG",colnames(counts))]
+f <- factors[rownames(factors) %in% colnames(df),]
+design <- model.matrix(~Caste,data=droplevels(f))
+b <- EdgeR(df,design,2)
+
+d2 <- melt(dfAll[,c(1,30:35,27,48)],id.vars=c("Gene","PS2","midF"))
+d2$tissue="Larva"
+d2$tissue[grepl("WG",d2$variable)]="WorkerGaster"
+d2$tissue[grepl("WH",d2$variable)]="WorkerHead"
+d2$connection.type = "within"
+d2$connection.type[grepl("between",d2$variable)]="between"
+d3 <- dcast(d2, PS2+tissue+Gene+midF ~ connection.type)
+
+ggplot(d3, aes(x=within,y=between,color=PS2,shape=tissue))+
+  geom_point()
+
+d4 <- ddply(d3,~PS2+tissue,summarise,
+            meanB = mean(between),
+            c1B = quantile(bootMean(between,1000),0.025),
+            c2B = quantile(bootMean(between,1000),0.975),
+            meanW = mean(within),
+            c1W = quantile(bootMean(within,1000),0.025),
+            c2W = quantile(bootMean(within,1000),0.975))
+
+ggplot(d4[!is.na(d4$PS2),],aes(x=meanW,y=meanB,color=PS2,shape=tissue))+
+  geom_point(size=3)+
+  geom_errorbar(aes(ymin=c1B,ymax=c2B))+
+  geom_errorbarh(aes(xmin=c1W,xmax=c2W))+
+  theme_bw()
+
+d3$SI = d3$between - d3$within
+ggplot(d3[!is.na(d3$PS2),],aes(x=tissue,y=SI,color=PS2))+geom_boxplot()
+ggplot(d3[!is.na(d3$PS2),],aes(x=SI,y=midF,color=PS2,shape=tissue))+geom_point()
+
+d4 <- ddply(d3,~PS2+tissue,summarise,
+            meanSI = mean(SI),
+            c1B = quantile(bootMean(SI,1000),0.025),
+            c2B = quantile(bootMean(SI,1000),0.975),
+            meanF = mean(midF),
+            c1W = quantile(bootMean(midF,1000),0.025),
+            c2W = quantile(bootMean(midF,1000),0.975))
+
+ggplot(d4[!is.na(d4$PS2),],aes(x=meanSI,y=meanF,color=PS2,shape=tissue))+
+  geom_point(size=3)+
+  geom_errorbarh(aes(xmin=c1B,xmax=c2B))+
+  geom_errorbar(aes(ymin=c1W,ymax=c2W))+
+  theme_bw()
+
+dOld = d[d$PS2=="cellular",]
+
+
