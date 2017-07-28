6b3b6c5e6a83c66d7e683c7d36a5e00571b39487
mrwarner09 <mrwarner09@gmail.com>
2017-07-28 10:24:08 -0400

    Collecting big batch of runs

--- collect_parallel.R
+++ collect_parallel.R
@@ -2,7 +2,7 @@
 library(plyr)
 
 #Load in results of networks with top 1000 connected genes
-setwd("~/Results")
+setwd("~/Results/bigboot")
 
 collate <- function(name){
   files <- list.files(pattern=paste(name,".*RData",sep=""))
@@ -28,7 +28,7 @@ collate <- function(name){
   WRsum$targReg =with(WRsum,paste0(regulatory.gene,target.gene))
   #Calculate "socialility index" as mean conn outside individual - mean conn inside tissue
   
-  write.csv(WRsum,file=paste(name,"WRsum.csv",sep=""))
+  #write.csv(WRsum,file=paste(name,"WRsum.csv",sep=""))
   WRsoc = data.frame(gene = unique(gsub(".*_","",WRsum$regulatory.gene)))
   WRsoc$Lwithin=WRsoc$Lbetween=WRsoc$WHwithin=WRsoc$WHbetween=WRsoc$WGwithin=WRsoc$WGbetween=0
   WRsocMax = WRsoc
@@ -40,25 +40,16 @@ collate <- function(name){
     WRsoc$WHbetween[i] = mean(d$meanW[(grepl("Larv",d$target.gene))&grepl("NurseH",d$regulatory.gene)])
     WRsoc$WGwithin[i] = mean(d$meanW[grepl("NurseG",d$target.gene)&grepl("NurseG",d$regulatory.gene)])
     WRsoc$WGbetween[i] = mean(d$meanW[(grepl("Larv",d$target.gene))&grepl("NurseG",d$regulatory.gene)])
-    WRsocMax$Lwithin[i] = max(d$meanW[grepl("Larv",d$target.gene)&grepl("Larv",d$regulatory.gene)])
-    WRsocMax$Lbetween[i] = max(d$meanW[(!grepl("Larv",d$target.gene))&grepl("Larv",d$regulatory.gene)])
-    WRsocMax$WHwithin[i] = max(d$meanW[grepl("NurseH",d$target.gene)&grepl("NurseH",d$regulatory.gene)])
-    WRsocMax$WHbetween[i] = max(d$meanW[(grepl("Larv",d$target.gene))&grepl("NurseH",d$regulatory.gene)])
-    WRsocMax$WGwithin[i] = max(d$meanW[grepl("NurseG",d$target.gene)&grepl("NurseG",d$regulatory.gene)])
-    WRsocMax$WGbetween[i] = max(d$meanW[(grepl("Larv",d$target.gene))&grepl("NurseG",d$regulatory.gene)])
   }
-  print(head(WRsocMax))
-  
   write.csv(WRsoc,paste(name,"NetSocialityDF.csv",sep=""))
-  write.csv(WRsocMax,paste(name,"NetSocialityDFmax.csv",sep=""))
 }
 
 collate("TopExprWorkerNet")
 collate("TopExprSexualNet")
 collate("TopExprRandomNet")
 
-collate("TopConnWorkerNet")
-collate("TopConnSexualNet")
-collate("TopConnRandomNet")
+#collate("TopConnWorkerNet")
+#collate("TopConnSexualNet")
+#collate("TopConnRandomNet")
 
 
