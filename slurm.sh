a39400fdf27e00832e0205222a362bef78125287
mrwarner09 <mrwarner09@gmail.com>
2017-09-28 15:04:35 -0400

    first commit

---
+++ slurm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash#
+#SBATCH -p general # partition (queue)
+#SBATCH -N 30 # number of nodes
+#SBATCH -n 120 # number of cores
+#SBATCH -t 0-24:00 # time (D-HH:MM)
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+#SBATCH --mail-type=END,FAIL # notifications for job done & fail
+#SBATCH --mail-user=warnerm@sas.upenn.edu # send-to addressfor i in {1..100000}; do
+Rscript collect.parallel.R
