1bd76c3f9688a0766ba8f82205284e8321be4ad9
mrwarner09 <mrwarner09@gmail.com>
2017-09-28 15:20:12 -0400

    fixing slurm

--- slurm.sh
+++ slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash#
 #SBATCH -p compute # partition (queue)
-#SBATCH -N 30 # number of nodes
-#SBATCH -n 120 # number of cores
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 1 # number of cores
 #SBATCH -t 0-24:00 # time (D-HH:MM)
 #SBATCH -o slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm.%N.%j.err # STDERR
