c0693cacb9756b10f1a18f7860e3c1a2c668b02a
mrwarner09 <mrwarner09@gmail.com>
2017-11-09 16:04:44 -0500

    Fixed errors from new factors file

--- RandomNetworks_parallel.R
+++ RandomNetworks_parallel.R
@@ -1,10 +1,11 @@
 ##RandomNetworks_parallel.R 
 ##this file produces many networks with a randomly chosen set of genes
 ##And then tabulates connection strength between chosen genes across all networks
-args <- commandArgs(TRUE)
+#args <- commandArgs(TRUE)
 #First arg is fpkm, second is factors file, third is sample subset, fourth is number of bootstraps, fifth is number of genes
-name <- args[1]
-library(parallel)
+#name <- args[1]
+
+name = "worker_1000_1000"
 library(plyr)
 
 load(paste(name,"InitialData.RData",sep="_")) #load initial codes, names, and fpkm
@@ -13,7 +14,7 @@ nGene = 10
 bootsPerCore = 500
 
 #This is better than full parallelization so we can deal with the try error
-runGenie <- function(run){
+runGenie <- function(){
   setwd("~/GENIE3_R_C_wrapper")
   source("~/GENIE3_R_C_wrapper/GENIE3.R")
   Results = list()
@@ -42,8 +43,8 @@ setInput <- function(codes,names){
     nSamp = getNsamp(codes,i)
     expr = list()
     for (j in 1:length(codes)){
-      f = factors[grepl(codes[j],rownames(factors))&factors$stage==i,]
-      samps = sample(rownames(f),nSamp,replace=FALSE) ##Want same number of samples per sample type
+      f = factors[grepl(codes[j],factors$sample.id)&factors$stage==i,]
+      samps = sample(f$sample.id,nSamp,replace=FALSE) ##Want same number of samples per sample type
       expr[[j]] = fpkm[,samps]
       expr[[j]]$tissue=names[j]
       expr[[j]]$gene = rownames(expr[[j]])
@@ -61,11 +62,11 @@ setInput <- function(codes,names){
 getNsamp <- function(codes,stage){
   nSamp = c()
   for (code in codes){
-    f = factors[grepl(code,rownames(factors))&factors$stage==stage,]
+    f = factors[grepl(code,factors$sample.id)&factors$stage==stage,]
     nSamp=c(nSamp,nrow(f))
   }
   return(min(nSamp))
 }
 
 input <- setInput(codes,names)
-RandomNetworks()
+runGenie()
