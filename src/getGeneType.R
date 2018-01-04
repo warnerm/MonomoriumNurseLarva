903d81a92377fcc631754695a9fe98637a837926
warnerm <mrwarner09@gmail.com>
2018-01-03 19:06:36 -0500

    enabled slurm parallelization

--- src/getGeneType.R
+++ src/getGeneType.R
@@ -12,24 +12,29 @@ tabType <- function(d){
   return(m2)
 }
 
+#Binomial test to see if gene is social
+socBinom <- function(i){
+  test = binom.test(m2$social[i],(m2$social[i]+m2$control[i]),p = 0.5)
+  m2$geneTypePval[i] = test$p.value
+  if (test$p.value < 0.05){ #Two sided test
+    if (test$estimate > 0.5) m2$geneType[i] = return(c(test$p.value,"social"))
+    else m2$geneType[i] = return(c(test$p.value,"control"))
+  }
+  return(c(test$p.value,'NS'))
+}
+
 #Determine whether or not genes are 'social' by lumping positive and negative connections
 #Given that some correlation disparity happened, use binomial test to say whether we see more social connections than control
 socGene <- function(m2){
   m2$social = m2$posSocial + m2$negSocial
   m2$control = m2$posControl + m2$negControl
-  m2$geneType = "NS"
-  m2$geneTypePval = 1
-  for (i in 1:nrow(m2)){
-    if (m2$social[i] > 0){
-      test = binom.test(m2$social[i],(m2$social[i]+m2$control[i]),p = 0.5)
-      m2$geneTypePval[i] = test$p.value
-      if (test$p.value < 0.05){ #Two sided test
-        if (test$estimate > 0.5) m2$geneType[i] = "social"
-        else m2$geneType[i] = "control"
-      }
-    }
-  }
-  return(m2)
+  df = data.frame(i = seq(1,nrow(m2),by=1))
+  sjob <- slurm_apply(socBinom, df, jobname = 'geneType',
+                      nodes = 4, cpus_per_node = 20, add_objects=c("data","genes","getCorCI","conType","compareCor"),submit = TRUE)
+  res <- get_slurm_out(sjob,outtype='raw') #get output as lists
+  res = ldply(res,rbind) #Each job is a row (i.e. connections with nurse genes for each larval gene)
+  colnames(res) = c('p.value','geneType')
+  return(cbind(m2,res))
 }
 
 allDat <- list(corQCH,corQCG,corCH,corCG,t(corQCH),t(corQCG),t(corCH),t(corCG),corRH,corRG) #Last two get Larva-Nurse social genes
